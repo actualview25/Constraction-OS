0cfbcf60a904e5b970755408ac621ae3a3e45a06
actualview25 <actual.view25@gmail.com>
2026-03-07 09:20:28 +0300

    Update GlobalEarthworksBOQ.js

--- Modules/BOQ/global/GlobalEarthworksBOQ.js
+++ Modules/BOQ/global/GlobalEarthworksBOQ.js
@@ -1,1 +1,20 @@
+export class GlobalEarthworksBOQ {
+    calculate(excavations, compactions) {
+        let totalExcavation = 0;
+        let totalCompaction = 0;
+        
+        excavations.forEach(ex => {
+            totalExcavation += ex.totalVolume;
+        });
+        
+        compactions.forEach(co => {
+            totalCompaction += co.totalVolume;
+        });
 
+        return {
+            totalExcavation: totalExcavation.toFixed(2),
+            totalCompaction: totalCompaction.toFixed(2),
+            netVolume: (totalExcavation - totalCompaction).toFixed(2)
+        };
+    }
+}
